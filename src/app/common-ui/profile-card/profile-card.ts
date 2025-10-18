2ef38929aed284af77231c339d0ae7e7396e8685
Vasya <VasyaPain2004@gmail.com>
2025-10-18 14:01:25 +0900

    first commit

---
+++ src/app/common-ui/profile-card/profile-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Tag } from "../tag/tag";
+
+@Component({
+  selector: 'app-profile-card',
+  imports: [Tag],
+  templateUrl: './profile-card.html',
+  styleUrl: './profile-card.scss'
+})
+export class ProfileCard {
+
+}
